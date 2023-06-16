16783aa56282fcc5b1d2d770752252ce71723b00
chiennguyen <chiennguyengusk@gmail.com>
2023-06-17 00:38:54 +0700

    chapter 1, bai 2

---
+++ JSB2/script.js
@@ -0,0 +1,5 @@
+// alert('Xin Chào Các Bạn Đến Với Chương Trình HTML của Chúng Tôi')
+// hoặc
+// console.log() là một hàm được sử dụng để ghi thông điệp ra màn hình console trong trình duyệt
+console.log('Xin Chào Các Bạn Đến Với Chương Trình JS của Chúng Tôi');
+console.log('Xin Chào Các Bạn');
